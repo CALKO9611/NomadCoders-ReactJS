551234d177346eec1e346bb2e9d935d51ce4497b
CALKO9611 <kimd9669@naver.com>
2023-07-29 18:07:27 +0900

    #10.2 Dynamic Component Generation

--- movie_app_2019/src/App.js
+++ movie_app_2019/src/App.js
@@ -1,17 +1,49 @@
 import React from "react";
 
-function Food({ favorite }) {
-  return <h1>I like {favorite}</h1>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "https://i.namu.wiki/i/3u7KQ8jVXWJMJpioMNBo5bFb7NrMd1jUbJrGXy99vSgh57D2w0BHG3RlEnawqyRlfAuTihp37sPnlCKX3IlKNg.webp",
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://i.namu.wiki/i/SdI6mHzKiWg-yJEqBpWhaACTth0ZSU_qSjhHCnQp5OxqwVl-PRIzql6bkmp1tm0YaHxjDoTbt-nBtiSE4nhqbQ.webp",
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "https://i.namu.wiki/i/dgjXU86ae29hDSCza-L0GZlFt3T9lRx1Ug9cKtqWSzMzs7Cd0CN2SzyLFEJcHVFviKcxAlIwxcllT9s2sck0RA.jpg",
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://i.namu.wiki/i/gMSvGksMaKPdznDSM-QxYwYGN-K1zLqOzOpNSAfOwRlrlx6ZY61kOvHHeAHtk9RWrTnA1nnrPri_XkXSewEkpQ.webp",
+  },
+  {
+    name: "Kimbap",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Gimbap_%28pixabay%29.jpg/1024px-Gimbap_%28pixabay%29.jpg",
+  },
+];
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Food favorite="kimchi" />
-      <Food favorite="ramen" />
-      <Food favorite="samgiopsal" />
-      <Food favorite="chukumi" />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
