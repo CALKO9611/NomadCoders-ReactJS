b8217cceea197f1aa47fec087ccc0b74e6fc562c
CALKO9611 <kimd9669@naver.com>
2023-08-04 16:07:26 +0900

    #12.5 Cutting the summary

--- movie_app_2019/src/Movie.js
+++ movie_app_2019/src/Movie.js
@@ -6,7 +6,7 @@ function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div className="movie_data">
+      <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
         <ul className="movie__genres">
@@ -16,7 +16,7 @@ function Movie({ year, title, summary, poster, genres }) {
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
